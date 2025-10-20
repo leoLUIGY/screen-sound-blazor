e4e2ec151b70f7dc03ad06c31fd4817c5ff3db34
leoLUIGY <luigyleonidas@gmail.com>
2025-10-20 11:30:47 -0300

    first commit

---
+++ ScreenSound.WebAssembly/Services/MusicasAPI.cs
@@ -0,0 +1,33 @@
+﻿using ScreenSound.Shared.Modelos.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.WebAssembly.Services
+{
+    public class MusicasAPI
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<MusicasAPI> logger;
+
+        public MusicasAPI(IHttpClientFactory factory, ILogger<MusicasAPI> logger)
+        {
+            _httpClient = factory.CreateClient("API");
+            this.logger = logger;
+        }
+
+        public async Task<ICollection<MusicaResponse>> GetMusicasAsync()
+        {
+            try
+            {
+                logger.LogInformation("Iniciando a chamada para obter as músicas.");
+
+                var result = await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+                logger.LogInformation("Chamada concluída com sucesso. Total de músicas obtidas: {Count}", result?.Count ?? 0);
+
+                return result;
+            }catch (Exception ex)
+            {
+                logger.LogError(ex, "Erro ao obter as músicas.");
+                throw;
+            }
+    }
+}
